0599baaf6ea154e1fa6b9c92afadb469908f69a1
valderramae08-hub <valderramae08@gmail.com>
2026-04-30 19:02:42 -0500

    cambio4

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = new mysqli(" mysql-aldrinn.alwaysdata.net", "aldrinn_db", "", "gestion_envios");
+$conexion = new mysqli(" mysql-aldrinn.alwaysdata.net", "aldrinn_db", "clase1234", "gestion_envios");
 
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
